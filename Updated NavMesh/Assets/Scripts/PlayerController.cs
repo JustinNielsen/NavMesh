f2964b5b988e7f43ef147cc56c14ae73277df11e
Justin Nielsen <nielsenjustin97@gmail.com>
2019-02-06 20:17:09 -0700

    Fixed line renderer issues/Refactored code

--- Updated NavMesh/Assets/Scripts/PlayerController.cs
+++ Updated NavMesh/Assets/Scripts/PlayerController.cs
@@ -36,7 +36,15 @@ public class PlayerController : MonoBehaviour
 
     public NavMeshPath path;
 
-    private Vector3 target;
+    private Vector3 movingTarget;
+
+    private Vector3 clickedTarget;
+
+    private bool isMoving = false;
+
+    private float pathLength;
+
+    private LineRenderer line;
 
     private void Start()
     {
@@ -57,6 +65,9 @@ public class PlayerController : MonoBehaviour
 
         //Initializes the path
         path = new NavMeshPath();
+
+        //Initialize the linerenderer
+        line = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -78,21 +89,32 @@ public class PlayerController : MonoBehaviour
             {
                 rb.velocity = Vector3.zero;
                 NavMeshMovement();
+
+                //Check if the player is done moving
+                if (maxDistance < 0.5f && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0 && Vector3.Distance(transform.position, agent.destination) <= 1f)
+                {
+                    //Sets the maxDistance back to 10 but doesn't update the scele of the range indicator yet.
+                    maxDistance = 10.0f;
+                    //Switch Turns
+                    turn.SwitchTurn();
+                }
+
+                if (Vector3.Distance(transform.position, agent.destination) <= 1f)
+                {
+                    //Sets isMoving to false
+                    isMoving = false;
+                }
+
+
             }
             else //if navMesh = false use keyboard movement
             {
                 //Apply forces to rigidbody
                 rb.velocity = moveVelocity;
+                isMoving = false;
             }
 
-            //Check if the player is done moving
-            if (maxDistance < 0.5f && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0 && Vector3.Distance(transform.position, agent.destination) <= 1f)
-            {
-                //Sets the maxDistance back to 10 but doesn't update the scele of the range indicator yet.
-                maxDistance = 10.0f;
-                //Switch Turns
-                turn.SwitchTurn();
-            }
+            
         }
     }
 
@@ -101,10 +123,10 @@ public class PlayerController : MonoBehaviour
         //Sets the scale of the movement range indicator
         //movementRange.transform.localScale = new Vector3(maxDistance * 2 - 0.5f, 0.01f, maxDistance * 2 - 0.5f);
 
-        if(target != null)
+        if(clickedTarget != null)
         {
             //Updates the path
-            NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            NavMesh.CalculatePath(transform.position, clickedTarget, NavMesh.AllAreas, path);
             //Draws updated path
             DrawPath(path, 3);
         }
@@ -118,12 +140,25 @@ public class PlayerController : MonoBehaviour
             //Defines which layers to ignore with the raycast
             int layerMask = 1 << 10;
 
-            ShootRayAndMove(ray, layerMask);
+            //sets clicked target to the location the ray hits
+            ShootRayClicked(ray, layerMask);
         }
 
+        //Triggers if the player isn't moving
+        if (!isMoving)
+        {
+            //Convertn mousePosition from a screen point to a ray
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            //Defines which layers to ignore with the raycast
+            int layerMask = 1 << 10;
+
+            //sets clicked target to the location the ray hits
+            movingTarget = ShootRay(ray, layerMask);
+        }
     }
 
-    private void ShootRayAndMove(Ray ray, int layerMask)
+    private Vector3 ShootRay(Ray ray, int layerMask)
     {
         //Declare a hit variable 
         RaycastHit hit;
@@ -131,35 +166,22 @@ public class PlayerController : MonoBehaviour
         //Check if the point the player clicked is possible to move to
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
         {
-            //Get the distance of the point from the player
-            Vector3 distance = hit.point - transform.position;
-
-            //Sets the target
-            target = hit.point;
+            //Set clicked target to the hit point
+            clickedTarget = hit.point;
 
             //Creates a NavMeshPath
             NavMesh.CalculatePath(transform.position, hit.point, NavMesh.AllAreas, path);
 
+            //Calculate the length of the path
+            pathLength = CalculatePathLength(path);
+
             //If the point clicked is within the max move distance then place a waypoint and move towards it
-            if (CalculatePathLength(path) < maxDistance)
+            if (pathLength < maxDistance)
             {
                 //Draws the path
                 DrawPath(path, 1);
 
-                //Check if a waypoint is already placed. If so Destroy it.
-                if (waypoint != null)
-                {
-                    Destroy(waypoint);
-                }
-
-                //Place a waypoint
-                waypoint = Instantiate(waypointPrefab, hit.point, Quaternion.identity);
-
-                //Move to clicked point
-                agent.SetDestination(hit.point);
-
-                //Subtract the distance moved from the maxDistance
-                maxDistance -= distance.magnitude;
+                Debug.Log("In Range");
 
                 //change the size of the movement range circle to coreleate with the max distance
                 //movementRange.transform.localScale = new Vector3(maxDistance * 2 - 0.5f, 0.01f, maxDistance * 2 - 0.5f);
@@ -172,7 +194,63 @@ public class PlayerController : MonoBehaviour
                 DrawPath(path, 2);
             }
 
-            Debug.Log("Path Length: " + CalculatePathLength(path).ToString("n2"));
+            //Debug.Log("Path Length: " + pathLength.ToString("n2"));
+            //Debug.Log("Max Distance: " + maxDistance);
+        }
+
+        return hit.point;
+    }
+
+    private void ShootRayClicked(Ray ray, int layerMask)
+    {
+        //Declare a hit variable 
+        RaycastHit hit;
+
+        //Check if the point the player clicked is possible to move to
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            //Creates a NavMeshPath
+            NavMesh.CalculatePath(transform.position, hit.point, NavMesh.AllAreas, path);
+
+            pathLength = CalculatePathLength(path);
+
+            //If the point clicked is within the max move distance then place a waypoint and move towards it
+            if (pathLength < maxDistance)
+            {
+                //Draws the path
+                DrawPath(path, 1);
+
+                //Check if a waypoint is already placed. If so Destroy it.
+                if (waypoint != null)
+                {
+                    Destroy(waypoint);
+                }
+
+                //Place a waypoint
+                waypoint = Instantiate(waypointPrefab, clickedTarget, Quaternion.identity);
+
+                //Subtract the distance moved from the maxDistance
+                maxDistance -= pathLength;
+
+                //Move to clicked point
+                agent.SetDestination(clickedTarget);
+
+                //Set isMoving to true
+                isMoving = true;
+
+                //change the size of the movement range circle to coreleate with the max distance
+                //movementRange.transform.localScale = new Vector3(maxDistance * 2 - 0.5f, 0.01f, maxDistance * 2 - 0.5f);
+            }
+            else
+            {
+                Debug.Log("Out of Range");
+
+                //Draws the path
+                DrawPath(path, 2);
+            }
+
+            //Debug.Log("Path Length: " + pathLength.ToString("n2"));
+            //Debug.Log("Max Distance: " + maxDistance);
         }
     }
 
@@ -201,15 +279,17 @@ public class PlayerController : MonoBehaviour
 
     private void DrawPath(NavMeshPath meshPath, int isGood)
     {
-        LineRenderer line = GetComponent<LineRenderer>();
+        line.positionCount = 0;
 
         if (isGood == 1)
         {
             line.material.color = Color.green;
+            Debug.Log("Green");
         }
         else if (isGood == 2)
         {
             line.material.color = Color.red;
+            Debug.Log("Red");
         }
 
         line.positionCount = meshPath.corners.Length;
